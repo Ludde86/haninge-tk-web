5da129ca5853c024b4a2f74e2f244730d79c4abe
Ludde86 <bslbjorn@gmail.com>
2024-05-03 22:29:08 +0200

    fix color hero buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./pages/footer/Footer";
 import HeroPage from "./pages/hero/HeroPage";
+import LandingPage from "./pages/landing/LandingPage";
 import Navbar from "./pages/navbar/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <HeroPage />
+      <LandingPage />
       <Footer />
     </>
   );
