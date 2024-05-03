2a567c5145f5630c20f981171840cfb1e4e261ac
Ludde86 <bslbjorn@gmail.com>
2024-05-03 16:20:02 +0200

    fix media navbar hero

--- src/pages/hero/HeroPage.jsx
+++ src/pages/hero/HeroPage.jsx
@@ -2,14 +2,14 @@ import "./heroPage.css";
 
 const HeroPage = () => {
   return (
-    <div className="hero_container center-end-row">
-      <div className="hero_container-text center-start-col">
+    <div className="hero_container">
+      <div className="hero_container-text">
         <h2>Välkommen till</h2>
         <h1>HANINGE</h1>
         <h1>TENNISKLUBB</h1>
       </div>
 
-      <div className="hero_container-button center-around-row">
+      <div className="hero_container-button">
         <button>Boka bana</button>
         <button>Bli medlem</button>
       </div>
