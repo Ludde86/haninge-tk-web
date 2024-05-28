f46c48a3564f167efc9291439c9660380ebae44b
Ludde86 <bslbjorn@gmail.com>
2024-05-28 12:30:33 +0200

    fix navbar submenu

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,18 +1,66 @@
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
-    <div className="navbar_container-wrapper">
-      <div className="navbar_container">
-        <Link to={"tennisskolan"}>Tennisskola</Link>
-        <Link to={"tavling"}>Tävling</Link>
-        <Menu className="menu-btn" size={40} />
-        <Link to={"tennisskolan"}>Medlemsskap</Link>
-        <Link to={"tennisskolan"}>Boka bana</Link>
+    <>
+      <div className="navbar_container-wrapper">
+        <div
+          className="navbar_container"
+          style={{
+            height: navOpen && "20svh",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <Link className="navbar_container-link" to={"tennisskolan"}>
+              Tennisskola
+            </Link>
+            <Link className="navbar_container-link" to={"tavling"}>
+              Tävling
+            </Link>
+            <div />
+            <Link className="navbar_container-link" to={"tennisskolan"}>
+              Medlemsskap
+            </Link>
+            <Link className="navbar_container-link" to={"tennisskolan"}>
+              Boka bana
+            </Link>
+          </div>
+
+          <div
+            className="navbar_container-submenu"
+            style={{
+              opacity: navOpen ? 1 : 0,
+            }}
+          >
+            <Link className="navbar_container-link" to={"tennisskolan"}>
+              Vision & Värdegrund
+            </Link>
+            <Link className="navbar_container-link" to={"tennisskolan"}>
+              Kontrakt/strötider
+            </Link>
+
+            <div />
+            <Link className="navbar_container-link" to={"tavling"}>
+              Kontakt
+            </Link>
+            <Link className="navbar_container-link" to={"tennisskolan"}>
+              Hitta hit
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+
+      {!navOpen ? (
+        <Menu onClick={() => setNavOpen(true)} className="menu-btn" size={40} />
+      ) : (
+        <X onClick={() => setNavOpen(false)} className="menu-btn" size={40} />
+      )}
+    </>
   );
 };
 
